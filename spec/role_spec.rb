e2e40089a34bdfaa37e0249706b369a7c7e72e8a
Ben Olive <sionide21@gmail.com>
2014-01-17 08:03:01 -0500

    Change movie to role and stricter parsing

--- spec/role_spec.rb
+++ spec/role_spec.rb
@@ -1,25 +1,25 @@
 require 'role'
 
 describe IMDB::Role do
-  let(:movie) { IMDB::Role.parse 'EuroTrip (2004)  [Jenny]  <6>' }
+  let(:role) { IMDB::Role.new 'EuroTrip (2004)  [Jenny]  <6>' }
   describe '#title' do
     it "returns the movie title" do
-      expect(movie.title).to eq("EuroTrip")
+      expect(role.title).to eq("EuroTrip")
     end
   end
   describe '#year' do
     it "returns the year of release" do
-      expect(movie.year).to eq(2004)
+      expect(role.year).to eq(2004)
     end
   end
   describe '#character' do
     it "returns the character name" do
-      expect(movie.character).to eq("Jenny")
+      expect(role.character).to eq("Jenny")
     end
   end
   describe '#credit' do
     it "returns the billing position in credits" do
-      expect(movie.credit).to eq(6)
+      expect(role.credit).to eq(6)
     end
   end
 end
